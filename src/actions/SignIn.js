88d592d4c0374610a6729eaad2ce9be91bde3ac7
Debashis Sharma <DebashisS@rssoftware.com>
2019-10-25 19:12:23 +0530

    axios changes

--- src/actions/SignIn.js
+++ src/actions/SignIn.js
@@ -1,34 +1,36 @@
 import axios from 'axios';
- 
+import axiosInstance from '../axios/axios';
+import { sha256, sha224 } from 'js-sha256';
 const _signin = (login) => ({
     type: 'LOGIN_PASSWORD',
     login 
 });
  
+
 export const login = (loginData = {
     email: '',
     password: ''
 }) => {
     
     return (dispatch) => {
-        const login = {
+         const login = {
             email: loginData.userName,
             password: loginData.password
-        };
+        }; 
         
  
        
-        // return axios({
-        //     url: 'http://localhost:8081/auth?ts=1571936819267',
-        //     method: 'post',
-        //     headers: {
-        //         'X-Auth-Password': loginData.password,
-        //         'X-Auth-Username':loginData.userName,
-        //         'Content-Type': 'application/json'
-        //     }
-        //  })
+        return axiosInstance({
+            url: 'http://localhost:8081/auth?ts=1571936819267',
+            method: 'post',
+            headers: {
+                'X-Auth-Password': sha256(loginData.password),
+                'X-Auth-Username':loginData.userName,
+                'Content-Type': 'application/json'
+            }
+         })
 
-        return axios.post('https://reqres.in/api/login', login)
+        //return axios.post('https://reqres.in/api/login', login)
         // .then(result => {
         //     dispatch(_signin(result.data));
             
--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,5 +1,26 @@
 import axios from 'axios';
  
-export default axios.create({
+/* export default axios.create({
     baseURL: 'http://localhost:8080/api'
-});
+}); */
+
+const axiosInstance = axios.create({
+    baseURL: 'localhost:8080'
+  })
+/*   axiosInstance.interceptors.request.use(
+    request => requestHandler(request)
+  );
+  axiosInstance.interceptors.response.use(
+    response => successHandler(response),
+    error => errorHandler(error)
+  ) */
+axiosInstance.interceptors.request.use(config => {
+    // perform a task before the request is sent
+    console.log('Request was sent');
+  
+    return config;
+  }, error => {
+    // handle the error
+    return Promise.reject(error);
+  });
+  export default axiosInstance;
